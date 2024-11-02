3208f386779c00e4e574609b76d39aefba9892a0
Tushar <aabisht2006@gmail.com>
2024-11-02 16:08:46 +0530

    robots.txt rickroll updated

---
+++ src/app/robots.txt/page.tsx
@@ -0,0 +1,11 @@
+"use client";
+import React, { useEffect } from "react";
+
+const page = () => {
+  useEffect(() => {
+    location.href = "https://youtu.be/dQw4w9WgXcQ";
+  }, []);
+  return <div></div>;
+};
+
+export default page;
