5d1dd8e0b702bea3bc40b263bd47252646e5d9b3
Tushar <aabisht2006@gmail.com>
2025-02-26 00:39:22 +0530

    blog button added

--- src/app/components/tabs/BlogTab.tsx
+++ src/app/components/tabs/BlogTab.tsx
@@ -1,25 +1,35 @@
 import { AppContext } from "@/app/context/AppContext";
 import React, { useContext } from "react";
-import styles from "./BlogTab.module.css"
+import styles from "./BlogTab.module.css";
 
 const BlogTab = () => {
-  const { data } = useContext(AppContext)
+  const { data } = useContext(AppContext);
   const { blogs } = data;
-    
+
   return (
     <div>
-      {
-        blogs ?
-        blogs.map(blog => (
-          <div key={blog.name} className={styles.blog}>
-            <h4>{blog.name}</h4>
-            <p>{blog.about}</p>
-            <a href={blog.link} target="___blank">Read More</a>
-          </div>
-        ))
-        :
-        <p className={styles.text}>No events yet</p>
-      }
+      <div className={styles.blogTop}>
+        <h4>EHAX Blogs</h4>
+        <p>Full blogs from EHAX {":)"}</p>
+        <a href="/blog" target="___blank">
+         View All
+        </a>
+      </div>
+      <>
+        {blogs ? (
+          blogs.map((blog) => (
+            <div key={blog.name} className={styles.blog}>
+              <h4>{blog.name}</h4>
+              <p>{blog.about}</p>
+              <a href={blog.link} target="___blank">
+                Read More
+              </a>
+            </div>
+          ))
+        ) : (
+          <p className={styles.text}>No events yet</p>
+        )}
+      </>
     </div>
   );
 };
